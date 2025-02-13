2056b33ad304a95937905a72aae71a49e28d1d74
Rider7723 <riderh911@gmail.com>
2025-02-13 12:27:30 +0530

    Done

--- Q3.cpp
+++ Q3.cpp
@@ -8,13 +8,12 @@ int main()
     cin>>num;
     
     lastdigit=num%10;
-    
     while (num>=10)
     {
         num=num/10;
     }
     firstdigit=num;
-    
+
     cout<<"The sum of the first and last digit of the number is: "<<firstdigit + lastdigit<<endl;
     
     return 0;
